c5ab4a71f157b0badf6b84befffc1feab14b1882
Hgmoa <hugo.97.vk@gmail.com>
2017-03-06 11:05:51 +0100

    Carga Noticias

--- resources/scripts/noticias.js
+++ resources/scripts/noticias.js
@@ -1,7 +1,7 @@
 function crearLinkNoticia(noticia) {
-    console.log("Noticia Titular: " + noticia.attr("title"));
-    console.log("Noticia Año: " + noticia.attr("data-year"));
-    console.log("Noticia Mes: " + noticia.attr());
+    console.log("Noticia Titular: " + noticia.getAttribute("title"));
+    console.log("Noticia Año: " + noticia.getAttribute("data-year"));
+    console.log("Noticia Mes: " + noticia.getAttribute("data-month"));
 }
 
 $(document).ready(function () {
