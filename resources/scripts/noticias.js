9b6d1520a2e1118cae9b2edad0cc697501f37c08
Hgmoa <hugo.97.vk@gmail.com>
2017-03-06 11:04:20 +0100

    Carga Noticias

--- resources/scripts/noticias.js
+++ resources/scripts/noticias.js
@@ -1,13 +1,13 @@
-function cargaNoticia(noticia,year,month) {
-    console.log("Noticia Titular: " + noticia);
-    console.log("Noticia Año: " + year);
-    console.log("Noticia Mes: " + month);
+function crearLinkNoticia(noticia) {
+    console.log("Noticia Titular: " + noticia.attr("title"));
+    console.log("Noticia Año: " + noticia.attr("data-year"));
+    console.log("Noticia Mes: " + noticia.attr());
 }
 
 $(document).ready(function () {
     var noticias = $(".img_noticia");
     for (var i = 0; i < noticias.length; i++) {
         var noticia = noticias[i];
-        cargaNoticia(noticia.getAttribute("title"),noticia.getAttribute("data-year"),noticia.getAttribute("data-month"));
+        crearLinkNoticia(noticia);
     }
 });
