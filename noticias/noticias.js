44db574a12a7ccd4dda9b7cefe920109259b88b6
Hgmoa <hugo.97.vk@gmail.com>
2017-03-08 22:14:53 +0100

    Carga Noticias con JSON arreglado

--- noticias/noticias.js
+++ noticias/noticias.js
@@ -2,16 +2,21 @@ function cargaNoticia(noticia) {
     var attrYear = noticia.getAttribute("data-year");
     var attrMonth = noticia.getAttribute("data-month");
     var attrTitle = noticia.getAttribute("title").split(" ").join("_");
-    var direccion = "/"+attrYear+"/"+attrMonth+"/"+attrTitle+".json";
+    var direccion = "/noticias/"+attrYear+"/"+attrMonth+"/"+attrTitle+".json";
     console.log(direccion);
-    console.log(window.lo);
     $.getJSON(direccion,function (data) {
         console.log("Json recuperado: " + data.toString());
         var articulo = document.createElement("article");
         var titular = document.createElement("h1").innerHTML=data.titular;
-        var imagenTitular = document.createElement("img");
-        var parrafos;
-        for (var i=0; i <  data.parrafos.length; i++)
+        var imagenTitular = document.createElement("img").setAttribute("src",attrTitle+".png");
+
+        articulo.appendChild(titular).appendChild(imagenTitular);
+
+        var parrafos = data.parrafos;
+        $.each(parrafos, function (k,v) {
+            console.log(k+":"+v);
+        });
+
         $("#Noticia").appendChild(articulo);
     });
 }
