a131cca26614eed23ff35dc393f63599b4f12b19
Hgmoa <hugo.97.vk@gmail.com>
2017-03-09 01:39:07 +0100

    Carga Noticias con JSON arreglado

--- noticias/noticias.js
+++ noticias/noticias.js
@@ -1,8 +1,8 @@
 function cargaNoticia(noticia) {
     var idNoticia = document.getElementById("Noticia");
 
-    idNoticia.style.visibility="hidden";
-    idNoticia.innerHTML="";
+    idNoticia.style.visibility = "hidden";
+    idNoticia.innerHTML = "";
 
     var attrYear = noticia.getAttribute("data-year");
     var attrMonth = noticia.getAttribute("data-month");
@@ -10,27 +10,26 @@ function cargaNoticia(noticia) {
     var direccion = "/noticias/" + attrYear + "/" + attrMonth + "/" + attrTitle + ".json";
 
     $.getJSON(direccion, function (data) {
-        var articulo = document.createElement("article");
-        var titular = document.createElement("h1");
-        titular.innerHTML = data.titular;
-        //var imagenTitular = document.createElement("img");
-        //imagenTitular.setAttribute("src",attrTitle+".png");
+        if (data != undefined) {
+            var articulo = document.createElement("article");
+            var titular = document.createElement("h1");
+            titular.innerHTML = data.titular;
+            //var imagenTitular = document.createElement("img");
+            //imagenTitular.setAttribute("src",attrTitle+".png");
 
-        articulo.appendChild(titular);
-        //articulo.appendChild(imagenTitular);
+            articulo.appendChild(titular);
+            //articulo.appendChild(imagenTitular);
 
-        var parrafosJSON = data.parrafos;
-        $.each(parrafosJSON, function (k, v) {
-            var parrafo = document.createElement("p");
-            parrafo.innerHTML = v;
-            articulo.appendChild(parrafo);
-        });
-        idNoticia.appendChild(articulo);
-        console.log("Fin Metodo JSON");
+            var parrafosJSON = data.parrafos;
+            $.each(parrafosJSON, function (k, v) {
+                var parrafo = document.createElement("p");
+                parrafo.innerHTML = v;
+                articulo.appendChild(parrafo);
+            });
+            idNoticia.appendChild(articulo);
+            idNoticia.style.visibility = "visible";
+        }
     });
-    if (idNoticia.innerHTML!=""){
-        idNoticia.style.visibility="visible";
-    }
 }
 
 $(document).ready(function () {
